af14772d9de903fbf0da7aac654a9f4c4ce733f5
Andrea Splendiani <andrea@sgtp.net>
2013-01-21 23:49:58 +0000

    First Ajaxy directory browser. Dirty but works

--- src/main/java/net/metarelate/terminology/webedit/SearchPage.java
+++ src/main/java/net/metarelate/terminology/webedit/SearchPage.java
@@ -4,18 +4,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import net.metarelate.terminology.coreModel.TerminologySet;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.extensions.markup.html.repeater.tree.AbstractTree;
+import org.apache.wicket.extensions.markup.html.repeater.tree.DefaultNestedTree;
+import org.apache.wicket.extensions.markup.html.repeater.tree.ITreeProvider;
+import org.apache.wicket.extensions.markup.html.repeater.tree.NestedTree;
+import org.apache.wicket.extensions.markup.html.repeater.tree.content.CheckFolder;
+import org.apache.wicket.extensions.markup.html.repeater.tree.content.Folder;
+import org.apache.wicket.extensions.markup.html.repeater.tree.content.StyledLinkLabel;
+import org.apache.wicket.extensions.markup.html.repeater.tree.theme.HumanTheme;
+import org.apache.wicket.extensions.markup.html.tree.LinkIconPanel;
+import org.apache.wicket.Component;
+import org.apache.wicket.MarkupContainer;
+
+import java.lang.String;
+
 
 public class SearchPage extends SuperPage {
 	private static final long serialVersionUID = 1L;
@@ -23,11 +40,90 @@ public class SearchPage extends SuperPage {
 	public SearchPage(PageParameters parameters) {
 		super(parameters);
 		
+		// TODO this is the result panel, should get to be something different
 		final Label resultLabel=new Label("resultPanel","Nothing yet");
 		resultLabel.setOutputMarkupId(true);
 		add(resultLabel);
 		
 		
+		//TODO here we should add some form logic for search
+		
+		
+		// TODO here we have the register tree
+		/*
+		final AbstractTree registerTree=new MyDefaultNestedTree("registerTree",createRegRootModel()) {
+
+			@Override
+			protected Component newContentComponent(String id, IModel model) {
+				// TODO Auto-generated method stub
+				return new Folder(id,this,model) ;
+			}
+			
+		};
+		*/
+		final AbstractTree registerTree=new DefaultNestedTree("registerTree",createRegRootModel())  {
+			@Override
+			protected Component newContentComponent(final String id, final IModel model) {
+				return new Folder(id, this, model) {
+					private static final long serialVersionUID = 1L;
+
+				            /**
+				             * Always clickable.
+				             */
+				            @Override
+				            protected boolean isClickable()
+				            {
+				                return true;
+				            }
+
+				            @Override
+				            protected void onClick(final AjaxRequestTarget target)
+				            {
+				            	resultLabel.setDefaultModelObject(model.getObject().toString());
+				   		        target.add(resultLabel);
+				                // TODO ok, here we should link to search
+				            	
+				            }
+
+				            @Override
+				            protected boolean isSelected()
+				            {
+				                return false;
+				            }
+
+							@Override
+							protected Component newLabelComponent(String id,
+									IModel model) {
+								String myLabel=model.getObject().toString();
+								if(CommandWebConsole.myFactory.terminologySetExist(myLabel))
+									myLabel=CommandWebConsole.myFactory.getOrCreateTerminologySet(myLabel).getLabel(CommandWebConsole.myFactory.getOrCreateTerminologySet(myLabel).getLastVersion());
+									//TODO terrible!!!!! Must be cleaned up a bit
+								return new Label("label",myLabel);
+							}
+
+			
+
+							
+				     
+				
+				};
+			}
+		};
+		
+		
+		
+		add(registerTree);
+		
+
+		
+		
+		
+		
+		
+		
+
+		 
+		    
 		/*
 		AjaxLink openRegButton = new AjaxLink("openRegButton") {
 		    public void onClick(final AjaxRequestTarget target) {
@@ -38,7 +134,7 @@ public class SearchPage extends SuperPage {
 		add(openRegButton);
 		*/
 		
-
+		/*
 		TerminologySet[] termRoots=CommandWebConsole.myFactory.getRootCollections();
 		List<String> termRootsURIList=new ArrayList<String>();
 		for(int i=0;i<termRoots.length;i++) {
@@ -56,8 +152,91 @@ public class SearchPage extends SuperPage {
 		
 		listContainer.add(termRootsListView);
 		add(listContainer);
-	
+		*/
 		
+		
+	}
+	
+	
+	
+	/*
+	private abstract class TermRegStyledLinkLabel<T> extends StyledLinkLabel<T> {
+		public TermRegStyledLinkLabel(String id,IModel<T> model) {
+			super(id, model);
+			 MarkupContainer link = newLinkComponent("link", model);
+             link.add(STYLE_CLASS);
+             add(link);
+
+             link.add(newLabelComponent("label", model));
+			// TODO Auto-generated constructor stub
+		}
+	}
+	
+	private  class TermRegFolder<T> extends TermRegStyledLinkLabel<T> {
+		public TermRegFolder(String id, IModel<T> model) {
+			super(id, model);
+			
+		}
+
+		@Override
+		protected java.lang.String getStyleClass() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	
+    };
+    */
+	
+	private class MyDefaultNestedTree extends DefaultNestedTree {
+
+		public MyDefaultNestedTree(String id, ITreeProvider provider) {
+			super(id, provider);
+		}
+		
+	}
+
+	private ITreeProvider<String> createRegRootModel() {
+		return new ITreeProvider<String>(){
+
+			public void detach() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public Iterator<String> getChildren(String collection) {
+				ArrayList<String> resultList=new ArrayList<String>();
+				if(CommandWebConsole.myFactory.terminologySetExist(collection)) {
+					Set<TerminologySet> children=CommandWebConsole.myFactory.getOrCreateTerminologySet(collection).getCollections();
+					Iterator<TerminologySet> setIter=children.iterator();
+					while(setIter.hasNext()) {
+						resultList.add(setIter.next().getURI())
+;					}
+				}
+				return resultList.iterator();
+			}
+
+			public Iterator<String> getRoots() {
+				TerminologySet[] roots=CommandWebConsole.myFactory.getRootCollections();
+				ArrayList<String> rootList=new ArrayList<String>();
+				for(int i=0;i<roots.length;i++) {
+					rootList.add(roots[i].getURI());
+				}
+				return rootList.iterator();
+			}
+
+			public boolean hasChildren(String collection) {
+				if(!CommandWebConsole.myFactory.terminologySetExist(collection)) return false;
+				Set<TerminologySet> children=CommandWebConsole.myFactory.getOrCreateTerminologySet(collection).getCollections();
+				if(children.size()>0) return true;
+				else return false;
+			}
+
+			public IModel<String> model(String arg) {
+				return new Model<String>(arg);
+				
+			}
+			
+		};
 		
 	}
 
@@ -71,6 +250,7 @@ public class SearchPage extends SuperPage {
 		return "Just searching";
 	}
 	
+	/*
 	private class RegisterListView extends ListView<String> {
 		ListView<String> termRootsListViewlistToUpdate=null;
 		WebMarkupContainer viewPanel=null;
@@ -86,9 +266,7 @@ public class SearchPage extends SuperPage {
 		}
 		
 
-		/**
-		 * 
-		 */
+		
 		private static final long serialVersionUID = 1L;
 
 		@Override
@@ -128,7 +306,7 @@ public class SearchPage extends SuperPage {
 		}
 		
 	}
-	
+	*/
 	/*
 	private class TerminologySetDecoupler implements IModel {
 		private String setURI=null;
